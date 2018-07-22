133a35a92b97412d362c30bd71c40bc90df14619
nguyenthethanhit <40885705+nguyenthethanhit@users.noreply.github.com>
2018-07-22 16:46:48 +0700

    update bai2

--- src/bai2/bai2.cpp
+++ src/bai2/bai2.cpp
@@ -1,108 +1,119 @@
 #include<iostream>
-#include<fstream>
 #include<string>
-#include<cstring>
+#include<fstream>
+#include<queue>
 using namespace std;
-int TimKiem(char path[],char key[]){
-	char *b;
-	char a[250];
-	int xuathien=0;
-	int dong=1;
-	b=a;
-	fstream f(path,ios::in);
-	while(!f.eof()){
-			f.getline(a,250);
-			b=a;
-			while((b=strstr(b,key))!=NULL){
-				xuathien++;
-				cout<<"ln "<<dong<<", col "<<b-a<<endl;
-				b++;
-			}
-			dong++;
-		}
-	cout<<key<<" xuat hien: "<<xuathien<<" lan"<<endl;
-	f.close();
+//sử dụng hàm find() để tìm từ khóa và hàm replace() để thay thế từ khóa
+void TimKiem(string key,char path[]){
+    fstream f(path,ios::in);
+    fstream f2("output.txt",ios::out);
+    string a;
+    queue<int> ln;      //queue chứa các dòng
+    queue<int> col;     // chứa các vị trí của keyword
+    int dong=1;
+    while(!f.eof()){
+        getline(f,a);   // đọc từng dòng trong file
+        int i = a.find(key);
+        while(i!=-1){
+            ln.push(dong);  //push dòng vào ln
+            col.push(i);    // push vị trí xuất hiện của từ vào col
+            i = a.find(key,i+key.size()-1);
+        }
+        dong++;
+    }
+    // số lần xuất hiện của key = số lượng phần tử trong queue
+    cout<<key<<" xuat hien: "<<col.size()<<" lan"<<endl;
+    f2<<key<<" xuat hien: "<<col.size()<<" lan"<<endl;
+    while(!col.empty()){
+        cout<<"ln "<<ln.front()<<" ,col: "<<col.front()<<endl;
+        f2<<"ln "<<ln.front()<<" ,col: "<<col.front()<<endl;
+        ln.pop();
+        col.pop();
 
-	
+    }
+    f.close();
+    f2.close();
 }
-int ThayThe(char path[]){
-	fstream f(path,ios::in);
-	fstream f2("replace.txt",ios::out);
-	string a,b;
-	while(!f.eof()){
-		
-		getline(f,a);
-		b.insert(b.size(),a);
-		b.append("\n");
-		
-		
-	}
-	cout<<b<<endl;
-	string key;
-	cout<<"find key: "<<endl;
-	cin>>key;
-	if(b.find(key)==-1){
-		cout<<"not found"<<endl;
-	}
-	else{
-		string key2;
-		cout<<"new key: "<<endl;
-		cin>>key2;
-		int i = b.find(key);
-		while(i!=-1){
-			
-			b.replace(i ,key.size(),key2);
-			i = b.find(key, i + key.size());
-		}
-		f2<<b;
-		f2<<"replace "<<key<<" by "<<key2<<endl;
-		cout<<"replace successfully"<<endl;
-	}
-	f2.close();
-	return 0;
+void ThayThe(char path[],string b){
+    
+    string key, newkey;
+    cout<<"find key: "<<endl;
+    cin>>key;
+    key.append(" ");    //thêm khoảng trắng vào sau key để không tìm đến từ có chứa key bên trong vd: abcKEYcba
+    cout<<"new key"<<endl;
+    cin>>newkey;
+    newkey.append(" ");
+    fstream f(path,ios::out);
+    
+    fstream f2("output.txt",ios::out);
+   // đưa tất cả nội dung của file vào string b. sau đó thực hiện tìm kiếm
+    if(b.find(key)==-1){
+        cout<<"not found "<<key<<endl;
+
+    }else{
+        int i = b.find(key);    // vị trí đầu tiên của key trong b
+        while(i!=-1){
+            b.replace(i,key.size(),newkey); // thay thế key bằng newkey
+            i = b.find(key,i+key.size());   // vị trí tiếp theo của key trong string b
+        }
+        cout<<"replace "<<key<<" by "<<newkey<<" successfully"<<endl;
+        f<<b;   // ghi string b đã thay thế vào file input.txt
+        f2<<"replace "<<key<<" by "<<newkey<<" successfully"<<endl; //ghi lịch sử vào file output.txt
+    }
+    f.close();
+    f2.close();
+
+
 }
-	
-	
 
 int main(){
-	char path[100];
-	char key[20];
+    string key;
+    string a,b;
+    char path[20];
+    nhapfile:cout<<"nhap file: "<<endl;
+    cin>>path;
+    fstream f(path,ios::in);
+    // kiểm tra file có hợp lệ hay không
+    if(f.fail()){
+        cout<<"file khong hop le, vui long nhap lai"<<endl;
+        goto nhapfile;
+    }
+    else{
+        while(true){
+            cout<<"================ MENU ================"<<endl;
+            cout<<"1. Tim Kiem"<<endl;
+            cout<<"2. Thay The"<<endl;
+            cout<<"3. Thoat"<<endl;
+            int luachon;
+            nhaplai:cout<<"================ nhap lua chon ================"<<endl;
+            cin>>luachon;
+            if(luachon<1 || luachon>3){
+                cout<<"lua chon khong hop le, vui long nhap lai"<<endl;
+                goto nhaplai;
+            }
+            else{
+                switch(luachon){
+                    case 1: 
+                        cout<<"nhap key: "<<endl;
+                        cin>>key;
+                        key.append(" ");    //thêm khoảng trắng vào key để hàm find() chỉ tìm đúng từ key, không tìm từ xxxKEYxxx
+                        TimKiem(key,path);  // truyền key và đường dẫn file vào hàm TimKiem()
+                        break;
+                    case 2:
+                     while(!f.eof()){
+                        getline(f,a);
+                        b.append(a);
+                        b.append("\n");
+                    }
+                    // chuyển toàn bộ nội dung từ file sang string b, sau đó truyền vào hàm ThayThe()
+                        ThayThe(path,b);
+                        break;
+                    case 3:
+                        exit(0);
 
-
-	cout<<"nhap duong dan file"<<endl;
-	cin>>path;
-	fstream f(path,ios::in);
-	if(f.fail()){
-		cout<<"file khong hop le, vui long nhap lai"<<endl;
-	}
-	else{
-		while(true){
-			cout<<"====== MENU ======"<<endl;
-		cout<<"1. Tim Kiem"<<endl;
-		cout<<"2. Thay the"<<endl;
-		cout<<"3. thoat"<<endl;
-		int luachon;
-		cout<<"nhap lua chon: "<<endl;
-		cin>>luachon;
-		if(luachon<1 || luachon>3){
-			cout<<"vui long nhap lai"<<endl;
-		}
-		switch(luachon){
-			case 1: 
-				cout<<"nhap tu khoa: "<<endl;
-				cin>>key;
-				TimKiem(path,key);
-				break;
-			case 2:
-				ThayThe(path);
-				break;
-			case 3: 
-				exit(0);
-		}
-		}
-	
-	}
-	f.close();
-
-
+                }
+            }
+        }
+    }
+    return 0;
 }
